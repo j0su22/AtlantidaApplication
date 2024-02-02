b3bb98fb582a698836b7ec3d2d46acde1392cbaf
Josue Cortez <josue.cortez222@gmail.com>
2024-02-01 19:10:48 -0600

    ADD
    declarando en el program.cs, la conexion a la base y servicios

--- AtlantidaApplicationAPI/Program.cs
+++ AtlantidaApplicationAPI/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AtlantidaApplicationAPI.Services;
+using AtlantidaApplicationAPI.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Conection to Data Base
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 // Add services to the container.
+builder.Services.AddTransient<IService_AGENCIA, AGENCIA_Service>();
+builder.Services.AddTransient<IService_CARGO, CARGO_Service>();
+builder.Services.AddTransient<IService_CLIENTEXPRODUCTO, CLIENTEXPRODUCTO_Service>();
+builder.Services.AddTransient<IService_EJECUTIVO, EJECUTIVO_Service>();
+builder.Services.AddTransient<IService_PERSONA, PERSONA_Service>();
+builder.Services.AddTransient<IService_PRODUCTO, PRODUCTO_Service>();
+builder.Services.AddTransient<IService_TIPOTRANSACCION, TIPOTRANSACCION_Service>();
+builder.Services.AddTransient<IService_TRANSACCION, TRANSACCION_Service>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
